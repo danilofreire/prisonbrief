6d9ae649f2468158a87b73b46592375bbb1e0139
Danilo Freire <danilofreire@gmail.com>
2017-07-23 03:24:08 +0100

    :pencil:

--- R/wpb_tables_data.R
+++ R/wpb_tables_data.R
@@ -2,7 +2,20 @@
 #'
 #' A dataset containing information on prison population for all countries available.
 #'
-#'
+#' \itemize{
+#'   \item country \code{character}.
+#'   \item prison_population_total \code{integer}.
+#'   \item prison_population_rate \code{integer}.
+#'   \item female_prisoners \code{numeric}.
+#'   \item pre_trial_detainees \code{numeric}.
+#'   \item foreign_prisoners \code{numeric}.
+#'   \item occupancy_level \code{numeric}.
+#'   \item juvenile_prisoners \code{numeric}.
+#'   \item number_institutions \code{numeric}.
+#'   \item iso_a2 \code{character}.
+#'   \item geometry \code{sfc_MULTIPOLYGON}.
+#' }
+#' 
 #' @format A data frame with 226 rows and 11 variables.
 "wpb_tables_data"
 #> [1] "wpb_tables_data"
